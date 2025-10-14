SHAP added and corrected ROC score calculation

src/explainability.py
@@ -15,18 +15,26 @@ import random
 import lime
 from lime import lime_text
 from lime.lime_text import LimeTextExplainer
+import random
+import shap
+import matplotlib.pyplot as plt
+import numpy as np
+
+#set seeds for reprodicibility
+random.seed(42)
+np.random.seed(42)
+tf.random.set_seed(42)
 
 encoder = SentenceTransformer('all-MiniLM-L6-v2')
 
 def make_predict_fn(align_matrix, pca, model_base):
-    """
-    Returns a predict_fn that LIME can use, with all required dependencies baked in.
-    """
+    #predict_fn has to only take in the strings, but output probabilities
+    #would be easier to save the matrices and models to locations and then could build directly without this nested function!
     def predict_fn(texts):
         embeds = encoder.encode(texts, show_progress_bar=False)
         embeds_align = np.matmul(embeds, align_matrix)
         embeds_PCA = pca.transform(embeds_align)
-        logits = model_base.predict(embeds_PCA)
+        logits = model_base.predict(embeds_PCA, verbose=0)
         exp_logits = np.exp(logits)
         probs = exp_logits / np.sum(exp_logits, axis=1, keepdims=True)
 
@@ -38,39 +46,39 @@ def lime_test(X_pos_strings_test, predict_fn):
 
     explainer = LimeTextExplainer(class_names=['medical query', 'not medical query'])
 
-
-    # 2. Pick a random index from X_base_test (or your test text array)
     # Pick a random index from the test text array, not the PCA embeddings
     random_idx = random.randint(0, len(X_pos_strings_test) - 1)
-    example_text = X_pos_strings_test[random_idx]  # <-- raw string
+    example_text = X_pos_strings_test[random_idx]  # raw string
 
     probs = predict_fn([example_text])[0]
     predicted_class = int(np.argmax(probs))
 
-    # 3. Generate explanation
     exp = explainer.explain_instance(
         example_text,
-        predict_fn,        # your wrapped function that returns probabilities
+        predict_fn,        # wrapped function that returns probabilities
         num_features=10,   # number of words to show in explanation
         top_labels=2
     )
-    # print("exp local exp:",exp.local_exp)
-
-
-
+    
     print(f"Random index: {random_idx}")
     print(f"Example text: {example_text}")
     print(f"Predicted class index: {predicted_class}")
     print(f"Class probabilities: {probs}")
     print(f"Mapped class: {explainer.class_names[predicted_class]}")
 
-    # LIME explanation
+    #LIME explanation
     lime_list = exp.as_list(label=predicted_class)
     print("LIME explanation:", lime_list)
 
-    # # Save interactive HTML - but rendering bug!
-    exp.save_to_file('data/explainability/oi.html')
+    # # Save interactive HTML - but rendering bug so show twice!
+    exp.save_to_file(f"data/explainability/lime_text_index-{random_idx}.html")
+    return random_idx
 
-    return
 
 
+def SHAP_vis(predict_fn, Vis_X_test_string, random_idx):
+    explainer = shap.Explainer(predict_fn, masker=shap.maskers.Text())
+    shap_values = explainer([Vis_X_test_string[random_idx]])
+    html_str = shap.plots.text(shap_values[0], display=False)
+    with open(f"data/explainability/shap_text_index{random_idx}.html", "w") as f:
+        f.write(html_str)

src/main.py
@@ -3,7 +3,7 @@ from data import pre_process, embed_and_align, embed_and_align_p, PCA_to_reduce_
 from train import get_model, train_base_model, adv_epsilon_training, adv_hyperrectangles_training
 from metrics import print_metrics, confusion_matrix_display, generalisability_metric, robustness_metric
 from sentence_transformers import SentenceTransformer
-from explainability import lime_test, make_predict_fn
+from explainability import lime_test, make_predict_fn, SHAP_vis
 from pgd_attack import pgd_attack_epsilon, pgd_attack_hyperrectangles
 from perturbations import create_perturbations
 from hyperrectangles import load_hyperrectangles
@@ -13,7 +13,6 @@ from visualise import tsne_display, UMAP_display, data_balance_display, print_co
 #############################################
 #### Pre-processing and Visualising Data ####
 #############################################
-
 
 #Preprocess data: Train vs Test, and Pos (Pos medical query = 0) vs Neg (Neg medical query = 1) (this is needed to compute perturbations for positive class only later)
 X_pos_strings_train, Y_pos_class_train, X_pos_strings_test, Y_pos_class_test, X_neg_strings_train, Y_neg_class_train, X_neg_strings_test, Y_neg_class_test = pre_process()
@@ -94,103 +93,106 @@ generalisability_metric(model_base, X_base_train, Y_base_train, X_base_test, Y_b
 #hence this is created by a second predict_fn function which takes all the important matrices for SVD, PCA, the model and the chosen encoder
 #if anything is changed in model pipeline please review explainability script!
 
-predict_fn = make_predict_fn(align_matrix, data_pca1, model_base)
-lime_test(Vis_X_test_string, predict_fn)
+predict_fn= make_predict_fn(align_matrix, data_pca1, model_base)
+random_idx = lime_test(Vis_X_test_string, predict_fn)
+
+#trying to use SHAP and using the same random index as LIME
+SHAP_vis(predict_fn, Vis_X_test_string, random_idx)
 
 
-############################
-#### Adversarial Attack ####
-############################
+# ############################
+# #### Adversarial Attack ####
+# ############################
 
-######################
-#### Epsilon Ball ####
-######################
+# ######################
+# #### Epsilon Ball ####
+# ######################
 
-#Simple epsilon ball pgd attack based on Katya's code, but only on positive data like in ANTONIO
-X_adv = pgd_attack_epsilon(model_base, X_pos_train_PCA, Y_pos_class_train)
+# #Simple epsilon ball pgd attack based on Katya's code, but only on positive data like in ANTONIO
+# X_adv = pgd_attack_epsilon(model_base, X_pos_train_PCA, Y_pos_class_train)
 
-# Evaluate the model on the adversarial examples
-# Could I use print_metrics function here?
-#uses logits, but should perhaps convert to softmax probabilities
-y_pred_adv = np.argmax(model_base.predict(X_adv), axis=1)
-accuracy_adv = np.mean(y_pred_adv == Y_pos_class_train)
+# # Evaluate the model on the adversarial examples
+# # Could I use print_metrics function here?
+# #uses logits, but should perhaps convert to softmax probabilities
+# y_pred_adv = np.argmax(model_base.predict(X_adv), axis=1)
+# accuracy_adv = np.mean(y_pred_adv == Y_pos_class_train)
 
-#uses logits, but should perhaps convert to softmax probabilities
-y_pred_train = np.argmax(model_base.predict(X_pos_train_PCA), axis=1)
-accuracy_train = np.mean(y_pred_train == Y_pos_class_train)
+# #uses logits, but should perhaps convert to softmax probabilities
+# y_pred_train = np.argmax(model_base.predict(X_pos_train_PCA), axis=1)
+# accuracy_train = np.mean(y_pred_train == Y_pos_class_train)
 
-print(f'Accuracy on adversarial examples: {accuracy_adv:.4f}')
-print(f'Accuracy on original positive only trained examples: {accuracy_train:.4f}')
+# print(f'Accuracy on adversarial examples: {accuracy_adv:.4f}')
+# print(f'Accuracy on original positive only trained examples: {accuracy_train:.4f}')
 
-#created a robustness metric, same as generalisability but instead of for test data, uses adversarial data generated for positive X training data
-robustness_metric(model_base, X_base_train, Y_base_train, X_adv, Y_pos_class_train)
+# #created a robustness metric, same as generalisability but instead of for test data, uses adversarial data generated for positive X training data
+# robustness_metric(model_base, X_base_train, Y_base_train, X_adv, Y_pos_class_train)
 
-##########################
-#### Hyper-rectangles ####
-##########################
+# ##########################
+# #### Hyper-rectangles ####
+# ##########################
 
-#This is lifted from ANTONIO, but inside function all other types of perturbation training was removed - 'word' because of conflicts with requirements, vicuna for space
-X_train_pos_p, Y_train_pos_p, train_pos_index, X_train_neg_p, Y_train_neg_p, train_neg_index, X_test_pos_p, Y_test_pos_p, test_pos_index, X_test_neg_p, Y_test_neg_p, test_neg_index = create_perturbations(X_pos_strings_train, Y_pos_class_train, X_neg_strings_train, Y_neg_class_train, X_pos_strings_test, Y_pos_class_test, X_neg_strings_test, Y_neg_class_test, 'character')
+# #This is lifted from ANTONIO, but inside function all other types of perturbation training was removed - 'word' because of conflicts with requirements, vicuna for space
+# X_train_pos_p, Y_train_pos_p, train_pos_index, X_train_neg_p, Y_train_neg_p, train_neg_index, X_test_pos_p, Y_test_pos_p, test_pos_index, X_test_neg_p, Y_test_neg_p, test_neg_index = create_perturbations(X_pos_strings_train, Y_pos_class_train, X_neg_strings_train, Y_neg_class_train, X_pos_strings_test, Y_pos_class_test, X_neg_strings_test, Y_neg_class_test, 'character')
 
-#Embed and Align perturbed version but using SVD matrix from original base training data to compare
-Xp_pos_train_embed_align, Xp_pos_test_embed_align, Xp_neg_train_embed_align, Xp_neg_test_embed_align= embed_and_align_p(X_train_pos_p, X_train_neg_p, X_test_pos_p, X_test_neg_p, align_matrix)
-#PCA from original base training data to compare
-Xp_pos_train_PCA = data_pca1.transform(Xp_pos_train_embed_align)
-#generates hyperrectangles - lifted from ANTONIO code
-hyperrectangles = load_hyperrectangles(X_pos_train_embed_align, X_pos_train_PCA, Xp_pos_train_embed_align, Xp_pos_train_PCA, train_pos_index)
+# #Embed and Align perturbed version but using SVD matrix from original base training data to compare
+# Xp_pos_train_embed_align, Xp_pos_test_embed_align, Xp_neg_train_embed_align, Xp_neg_test_embed_align= embed_and_align_p(X_train_pos_p, X_train_neg_p, X_test_pos_p, X_test_neg_p, align_matrix)
+# #PCA from original base training data to compare
+# Xp_pos_train_PCA = data_pca1.transform(Xp_pos_train_embed_align)
+# #generates hyperrectangles - lifted from ANTONIO code
+# hyperrectangles = load_hyperrectangles(X_pos_train_embed_align, X_pos_train_PCA, Xp_pos_train_embed_align, Xp_pos_train_PCA, train_pos_index)
 
-#sanity check
-print("Number of hyperrectangles:", len(hyperrectangles))
+# #sanity check
+# print("Number of hyperrectangles:", len(hyperrectangles))
 
-n_samples = int(len(X_pos_train_PCA))
-#generating just attack data using the hyperrectangles - lifed from inside adversarial attack in ANTONIO code
-pgd_dataset_hyper, pgd_labels_inside_hyper = pgd_attack_hyperrectangles(model_base, hyperrectangles, n_samples)
-#uses logits, but should perhaps convert to softmax probabilities
-y_hyper_pred = np.argmax(model_base.predict(pgd_dataset_hyper), axis=1)
-accuracy_hyper_pred = np.mean(y_hyper_pred == pgd_labels_inside_hyper)
+# n_samples = int(len(X_pos_train_PCA))
+# #generating just attack data using the hyperrectangles - lifed from inside adversarial attack in ANTONIO code
+# pgd_dataset_hyper, pgd_labels_inside_hyper = pgd_attack_hyperrectangles(model_base, hyperrectangles, n_samples)
+# #uses logits, but should perhaps convert to softmax probabilities
+# y_hyper_pred = np.argmax(model_base.predict(pgd_dataset_hyper), axis=1)
+# accuracy_hyper_pred = np.mean(y_hyper_pred == pgd_labels_inside_hyper)
 
 
-# Evaluate the model on the adversarial examples
-# Could I use print_metrics function here?
-print(f'Accuracy on adversarial attack from hyper rectangle examples: {accuracy_hyper_pred:.4f}')
-#created a robustness metric, same as generalisability but instead of for test data, uses adversarial data generated for positive X training data
-robustness_metric(model_base, X_base_train, Y_base_train, pgd_dataset_hyper, pgd_labels_inside_hyper)
+# # Evaluate the model on the adversarial examples
+# # Could I use print_metrics function here?
+# print(f'Accuracy on adversarial attack from hyper rectangle examples: {accuracy_hyper_pred:.4f}')
+# #created a robustness metric, same as generalisability but instead of for test data, uses adversarial data generated for positive X training data
+# robustness_metric(model_base, X_base_train, Y_base_train, pgd_dataset_hyper, pgd_labels_inside_hyper)
 
-##############################
-#### Adversarial Training ####
-##############################
+# ##############################
+# #### Adversarial Training ####
+# ##############################
 
-######################
-#### Epsilon Ball ####
-######################
+# ######################
+# #### Epsilon Ball ####
+# ######################
 
-#lifted from Katya's lab
-model_adv_eps = get_model()
-#this is dropping accuracy for train but the accuracy for test is high
-model_adv_eps = adv_epsilon_training(model_adv_eps, train_base_dataset, test_base_dataset)
-#lets evaluate on clean train data
-y_adv_pred_pos_train = np.argmax(model_adv_eps.predict(X_pos_train_PCA), axis=1)
-accuracy_train_adv_pred_pos = np.mean(y_adv_pred_pos_train == Y_pos_class_train)
+# #lifted from Katya's lab
+# model_adv_eps = get_model()
+# #this is dropping accuracy for train but the accuracy for test is high
+# model_adv_eps = adv_epsilon_training(model_adv_eps, train_base_dataset, test_base_dataset)
+# #lets evaluate on clean train data
+# y_adv_pred_pos_train = np.argmax(model_adv_eps.predict(X_pos_train_PCA), axis=1)
+# accuracy_train_adv_pred_pos = np.mean(y_adv_pred_pos_train == Y_pos_class_train)
 
-y_adv_pred_train_eps = np.argmax(model_adv_eps.predict(X_base_train), axis=1)
-accuracy_train_adv_pred_eps = np.mean(y_adv_pred_train_eps == Y_base_train)
+# y_adv_pred_train_eps = np.argmax(model_adv_eps.predict(X_base_train), axis=1)
+# accuracy_train_adv_pred_eps = np.mean(y_adv_pred_train_eps == Y_base_train)
 
-print(f'Accuracy on original positive only trained examples: {accuracy_train_adv_pred_pos:.4f}')
-print(f'Accuracy on original total trained examples: {accuracy_train_adv_pred_eps:.4f}')
+# print(f'Accuracy on original positive only trained examples: {accuracy_train_adv_pred_pos:.4f}')
+# print(f'Accuracy on original total trained examples: {accuracy_train_adv_pred_eps:.4f}')
 
-##########################
-#### Hyper-rectangles ####
-##########################
+# ##########################
+# #### Hyper-rectangles ####
+# ##########################
 
-#lifted from ANTONIO
-model_adv_hyper = get_model()
-model_adv_hyper = adv_hyperrectangles_training(model_adv_hyper, train_base_dataset, test_base_dataset, hyperrectangles, n_samples)
-#lets evaluate on clean train data
-y_adv_hyper_pred_pos_train = np.argmax(model_adv_hyper.predict(X_pos_train_PCA), axis=1)
-accuracy_train_adv_hyper_pred_pos = np.mean(y_adv_hyper_pred_pos_train == Y_pos_class_train)
+# #lifted from ANTONIO
+# model_adv_hyper = get_model()
+# model_adv_hyper = adv_hyperrectangles_training(model_adv_hyper, train_base_dataset, test_base_dataset, hyperrectangles, n_samples)
+# #lets evaluate on clean train data
+# y_adv_hyper_pred_pos_train = np.argmax(model_adv_hyper.predict(X_pos_train_PCA), axis=1)
+# accuracy_train_adv_hyper_pred_pos = np.mean(y_adv_hyper_pred_pos_train == Y_pos_class_train)
 
-y_adv_hyper_pred_train = np.argmax(model_adv_hyper.predict(X_base_train), axis=1)
-accuracy_train_adv_hyper_pred = np.mean(y_adv_hyper_pred_train == Y_base_train)
+# y_adv_hyper_pred_train = np.argmax(model_adv_hyper.predict(X_base_train), axis=1)
+# accuracy_train_adv_hyper_pred = np.mean(y_adv_hyper_pred_train == Y_base_train)
 
-print(f'Hyper Accuracy on original positive only trained examples: {accuracy_train_adv_hyper_pred_pos:.4f}')
-print(f'Hyper Accuracy on original total trained examples: {accuracy_train_adv_hyper_pred:.4f}')
+# print(f'Hyper Accuracy on original positive only trained examples: {accuracy_train_adv_hyper_pred_pos:.4f}')
+# print(f'Hyper Accuracy on original total trained examples: {accuracy_train_adv_hyper_pred:.4f}')

src/metrics.py
@@ -11,6 +11,11 @@ from sklearn.metrics import accuracy_score, balanced_accuracy_score, precision_s
 from sklearn.preprocessing import label_binarize
 import matplotlib.pyplot as plt
 from sklearn.model_selection import cross_validate
+import random
+#set seeds for reprodicibility
+random.seed(42)
+np.random.seed(42)
+tf.random.set_seed(42)
 
 def print_metrics(model, X_test, Y_test):
     # Get predicted probabilities for all classes
@@ -41,11 +46,11 @@ def print_metrics(model, X_test, Y_test):
     fpr = dict()
     tpr = dict()
     roc_auc = dict()
-
+    print(y_pred_prob_softmax)
     y_score = y_pred_prob_softmax[:,0]
 
     fpr, tpr, thresholds = roc_curve(Y_test, y_score, pos_label=0)
-    roc_auc = roc_auc_score(Y_test, y_score)
+    roc_auc = 1 - roc_auc_score(Y_test, y_score) #need 1 - as automatically it assumed region of interest is 1, not 0
     print(f"ROC score for positive medical query class: {roc_auc}")
 
     # Plot the ROC curve for each class